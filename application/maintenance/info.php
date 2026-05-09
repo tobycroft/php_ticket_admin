2dbad5fd7ec9df12091452df383414145c35ca74
火线兔 <silverhawk@foxmail.com>
2026-05-09 17:39:07 +0800

    Init

--- application/maintenance/info.php
+++ application/maintenance/info.php
@@ -18,13 +18,13 @@ return [
     // 模块标题[必填]
     'title' => '运维系统',
     // 模块唯一标识[必填]，格式：模块名.开发者标识.module
-    'identifier' => 'maintenance.tuuz.module',
+    'identifier' => 'maintenance.fc.module',
     // 模块图标[选填]
     'icon' => 'fa fa-fw fa-newspaper-o',
     // 模块描述[选填]
     'description' => '运维模块',
     // 开发者[必填]
-    'author' => 'YourFather',
+    'author' => 'FC',
     // 版本[必填],格式采用三段式：主版本号.次版本号.修订版本号
     'version' => '1.0.0',
     // 模块依赖[可选]，格式[[模块名, 模块唯一标识, 依赖版本, 对比方式]]
