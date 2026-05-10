aa48c288ff18cd911fc746fc3c993f36183283e4
Tuuz <2539@tuuz.cn>
2026-05-10 21:49:53 +0800

    TuuzPC

--- application/maintenance/admin/Event.php
+++ application/maintenance/admin/Event.php
@@ -18,12 +18,15 @@ class Event extends Admin
         $data_list = EventAction::getList($map);
 
         $is_closed_list = EventAction::getIsClosedList();
+        $is_canceled_list = EventAction::getIsCanceledList();
 
         foreach ($data_list as &$item) {
             $item['is_closed_text'] = isset($is_closed_list[$item['is_closed']]) ? $is_closed_list[$item['is_closed']] : '';
+            $item['is_canceled_text'] = isset($is_canceled_list[$item['is_canceled']]) ? $is_canceled_list[$item['is_canceled']] : '';
             $item['start_time_text'] = $item['start_time'] ? date('Y-m-d H:i:s', $item['start_time']) : '';
             $item['end_time_text'] = $item['end_time'] ? date('Y-m-d H:i:s', $item['end_time']) : '';
-            $item['can_close'] = ($item['receiver_id'] == UID || $item['sender_id'] == UID) && !$item['is_closed'];
+            $item['can_close'] = ($item['receiver_id'] == UID || $item['sender_id'] == UID) && !$item['is_closed'] && !$item['is_canceled'];
+            $item['can_cancel'] = $item['sender_id'] == UID && !$item['is_closed'] && !$item['is_canceled'];
         }
 
         return ZBuilder::make('table')
@@ -38,11 +41,12 @@ class Event extends Admin
                 ['customer_name', '对接客户'],
                 ['start_time_text', '开始时间'],
                 ['end_time_text', '结束时间'],
+                ['is_canceled_text', '状态'],
                 ['is_closed', '结单状态', 'switch', '', ['未结单', '已结单']],
                 ['right_button', '操作', 'btn']
             ])
-            ->addTopButtons('add,delete')
-            ->addRightButtons(['detail', 'close' => ['title' => '结单', 'icon' => 'fa-check-circle', 'class' => 'btn btn-xs btn-success', 'condition' => 'can_close']])
+            ->addTopButtons('add')
+            ->addRightButtons(['detail', 'close' => ['title' => '结单', 'icon' => 'fa-check-circle', 'class' => 'btn btn-xs btn-success', 'href' => url('close', ['id' => '__id__']), 'condition' => 'can_close'], 'cancel' => ['title' => '作废', 'icon' => 'fa-trash', 'class' => 'btn btn-xs btn-danger', 'href' => url('cancel', ['id' => '__id__']), 'condition' => 'can_cancel']])
             ->setRowList($data_list)
             ->fetch();
     }
