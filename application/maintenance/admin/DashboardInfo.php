9bd6f76f6244884482f4e40ad3dc30d1c6a5c017
火线兔 <silverhawk@foxmail.com>
2026-05-13 13:48:07 +0800

    修改DashboardInfo为信息面板式显示：包含待接收交接、公开交接、今日交接状态、待处理工单

--- application/maintenance/admin/DashboardInfo.php
+++ application/maintenance/admin/DashboardInfo.php
@@ -3,7 +3,6 @@
 namespace app\maintenance\admin;
 
 use app\admin\controller\Admin;
-use app\common\builder\ZBuilder;
 use app\maintenance\action\HandoverAction;
 use app\maintenance\action\EventAction;
 
@@ -11,32 +10,19 @@ class DashboardInfo extends Admin
 {
     public function index()
     {
-        $unclaimed_handovers = HandoverAction::getUnclaimedHandovers();
+        $my_unfinished_count = HandoverAction::getMyUnfinishedHandoverCount();
+        $unassigned_count = HandoverAction::getUnassignedHandoverCount();
+        $has_handover_today = HandoverAction::hasHandoverToday();
         
         $my_events = EventAction::getMyEvents(UID);
-        
-        $unclaimed_count = count($unclaimed_handovers);
         $my_event_count = $my_events->total();
 
-        foreach ($unclaimed_handovers as &$handover) {
-            $handover['is_for_me'] = $handover['default_receiver_id'] == UID;
-            $handover['can_force_receive'] = true;
-        }
+        $this->assign('my_unfinished_count', $my_unfinished_count);
+        $this->assign('unassigned_count', $unassigned_count);
+        $this->assign('has_handover_today', $has_handover_today);
+        $this->assign('my_event_count', $my_event_count);
+        $this->assign('page_title', '信息面板');
 
-        return ZBuilder::make('table')
-            ->setPageTitle('信息面板')
-            ->setPageTips('欢迎使用运维系统信息面板', 'info')
-            ->addColumns([
-                ['id', 'ID'],
-                ['title', '交接标题'],
-                ['creator_name', '创建人'],
-                ['default_receiver_name', '默认接收人'],
-                ['create_time', '创建时间'],
-                ['right_button', '操作', 'btn']
-            ])
-            ->addRightButtons(['receive' => ['title' => '接收交接', 'icon' => 'fa fa-handshake-o', 'class' => 'btn btn-xs btn-success', 'href' => url('Handover/receive', ['id' => '__id__'])], 'detail' => ['title' => '详情', 'icon' => 'fa fa-eye', 'href' => url('Handover/detail', ['id' => '__id__'])]])
-            ->setRowList($unclaimed_handovers)
-            ->setExtraHtml('<div class="row"><div class="col-md-6"><div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">待处理交接</h3></div><div class="panel-body"><div class="huge">' . $unclaimed_count . '</div><div>等待接收的交接数量</div></div></div></div><div class="col-md-6"><div class="panel panel-info"><div class="panel-heading"><h3 class="panel-title">我的工单</h3></div><div class="panel-body"><div class="huge">' . $my_event_count . '</div><div>正在处理的工单数量</div></div></div></div>')
-            ->fetch();
+        return $this->fetch('dashboard_info/index');
     }
 }
