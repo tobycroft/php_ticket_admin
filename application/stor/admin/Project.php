fa60d2275702547035a9514eda7e3e29aae67b41
火线兔 <silverhawk@foxmail.com>
2026-05-14 15:40:55 +0800

    refactor: 将renderProjectMaterials方法移到ProjectAction类中

--- application/stor/admin/Project.php
+++ application/stor/admin/Project.php
@@ -5,8 +5,7 @@ namespace app\stor\admin;
 use app\admin\controller\Admin;
 use app\common\builder\ZBuilder;
 use app\stor\model\ProjectModel;
-use app\stor\model\MaterialSnModel;
-use app\stor\model\MaterialModel;
+use app\stor\action\ProjectAction;
 
 class Project extends Admin
 {
@@ -118,23 +117,8 @@ class Project extends Admin
 
         $info = ProjectModel::getInfo($id);
 
-        $snList = MaterialSnModel::where('project_id', $id)->select();
-        
-        $materialMap = [];
-        $materials = [];
-        
-        foreach ($snList as $snItem) {
-            $materialId = $snItem['material_id'];
-            if (!isset($materialMap[$materialId])) {
-                $material = MaterialModel::getInfo($materialId);
-                $materialMap[$materialId] = $material;
-                $materials[$materialId] = [
-                    'material' => $material,
-                    'sns' => []
-                ];
-            }
-            $materials[$materialId]['sns'][] = $snItem['sn'];
-        }
+        $materials = ProjectAction::getProjectMaterials($id);
+        $materialsHtml = ProjectAction::renderProjectMaterials($materials);
 
         return ZBuilder::make('form')
             ->setPageTitle('项目详情')
@@ -144,34 +128,9 @@ class Project extends Admin
                 ['static', 'remark', '项目备注'],
                 ['static', 'create_time', '创建时间']
             ])
-            ->setExtraHtml($this->renderProjectMaterials($materials),'form_top')
+            ->setExtraHtml($materialsHtml, 'form_top')
             ->setFormData($info)
             ->fetch();
-    }
-
-    private function renderProjectMaterials($materials)
-    {
-        if (empty($materials)) {
-            return '<div class="block"><div class="block-header bg-gray-lighter"><h3 class="block-title"><i class="fa fa-cubes mr-5"></i>使用物料</h3></div><div class="block-content text-center py-20"><div class="alert alert-info">该项目暂无使用物料</div></div></div>';
-        }
-        
-        $html = '<div class="block"><div class="block-header bg-gray-lighter"><h3 class="block-title"><i class="fa fa-cubes mr-5"></i>使用物料</h3></div><div class="block-content">';
-        
-        foreach ($materials as $item) {
-            $material = $item['material'];
-            $sns = $item['sns'];
-            
-            $html .= '<div class="panel panel-default mb-10"><div class="panel-heading"><h4 class="panel-title">' . $material['name'] . '</h4></div><div class="panel-body"><div class="row"><div class="col-md-6"><p><strong>所属分类：</strong>' . $material['category_id'] . '</p><p><strong>存放位置：</strong>' . ($material['location'] ?: '-') . '</p><p><strong>单位：</strong>' . ($material['unit'] ?: '-') . '</p></div><div class="col-md-6"><p><strong>SN码列表：</strong></p><div class="tag-list">';
-            
-            foreach ($sns as $sn) {
-                $html .= '<span class="label label-default mr-2">' . $sn . '</span>';
-            }
-            
-            $html .= '</div></div></div></div>';
-        }
-        
-        $html .= '</div></div>';
-        return $html;
     }
 
     public function delete($ids = [])
