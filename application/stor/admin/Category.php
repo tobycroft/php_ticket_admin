5571ed3451ae7a90f83f1aa3c545475822352bc7
火线兔 <silverhawk@foxmail.com>
2026-05-14 11:48:40 +0800

    移除批量作废功能，只保留单条作废

--- application/stor/admin/Category.php
+++ application/stor/admin/Category.php
@@ -26,7 +26,7 @@ class Category extends Admin
                 ['status', '状态', 'switch'],
                 ['right_button', '操作', 'btn']
             ])
-            ->addTopButtons(['add', 'enable', 'disable', 'scrap' => ['title' => '作废', 'icon' => 'fa fa-trash', 'class' => 'btn btn-danger']])
+            ->addTopButtons('add,enable,disable')
             ->addRightButtons(['edit', 'scrap' => ['title' => '作废', 'icon' => 'fa fa-trash', 'class' => 'btn btn-xs btn-danger', 'href' => url('scrap', ['id' => '__id__'])]])
             ->setRowList($data_list)
             ->fetch();
