b04133d30e3e995d513fbd603ad4708ac0a13d8c
火线兔 <silverhawk@foxmail.com>
2026-05-14 12:17:47 +0800

    删除物料表(stor_material)中无用的code字段及代码中的相关引用

--- application/stor/admin/Material.php
+++ application/stor/admin/Material.php
@@ -29,7 +29,6 @@ class Material extends Admin
             ->setSearch(['name' => '物料名称', 'seller' => '销售方'])
             ->addColumns([
                 ['id', 'ID'],
-                ['code', '物料编码'],
                 ['name', '物料名称'],
                 ['seller', '销售方'],
                 ['purchase_date', '购入日期', 'date'],
@@ -79,7 +78,6 @@ class Material extends Admin
             ->setPageTitle('新增物料')
             ->addFormItems([
                 ['select', 'category_id', '所属分类', '必填', $category_options],
-                ['text', 'code', '物料编码', '必填'],
                 ['text', 'name', '物料名称', '必填'],
                 ['text', 'seller', '销售方'],
                 ['date', 'purchase_date', '购入日期'],
@@ -134,7 +132,6 @@ class Material extends Admin
             ->addFormItems([
                 ['hidden', 'id'],
                 ['select', 'category_id', '所属分类', '必填', $category_options],
-                ['text', 'code', '物料编码', '只读', '', 'disabled'],
                 ['text', 'name', '物料名称', '必填'],
                 ['text', 'seller', '销售方'],
                 ['date', 'purchase_date', '购入日期'],
